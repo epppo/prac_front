c7245664af990132790e916a706d4daedaa4f0da
epppo <amorosso@kakao.com>
2022-06-28 09:07:43 +0900

    no message

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import save from "./modules/saveList";
+import favorite from "./modules/favorite";
 
 const store = configureStore({
-  reducer: { save },
+  reducer: { save, favorite },
 });
 
 export default store;
